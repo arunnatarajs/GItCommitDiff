4dbf612930158d3e052682bc4794c41b73c47b30
arun <asaravanan@fleetstudio.com>
2021-11-25 11:37:51 +0530

    node server

--- dataPrinting.js
+++ dataPrinting.js
@@ -1,17 +1,6 @@
 function printing(n,data,res){
-    if(n==1){
-        // res.write(JSON.stringify(data));
-        res.write("<div align='right'> <h3>" + 
-        "Commited by : " + JSON.stringify(data.commit.committer.name) + "</h3> <br> <h3>" 
-        +"Commited on : " + JSON.stringify(data.commit.committer.date) + "</h3> <br> <h3>"
-        +"parent : " + JSON.stringify(data.parents[0].sha) + "</h3> </div>");
-
-    }
-
-   if(n==2){
-        res.write(`<h5>hello</h5>`);
-    }
-
+    // res.write(JSON.stringify(data));
+    res.send(data);
     res.end();
 }
 
